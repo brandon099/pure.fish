57a201f2782eac16e4f4b5e3b4b6fe24df65c99a
Brandon Clifford <brandon.clifford@vivint.com>
2016-05-27 00:04:47 -0600

    Add long running process run time

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -3,7 +3,35 @@ function _pwd_with_tilde
 end
 
 function _prompt_hostname
-  echo $USER"@"(hostname | cut -d . -f 1)
+  echo $USER"@"(cat /etc/hostname 2>/dev/null; or hostname | cut -d . -f 1)
+end
+
+function _format_time -d "Format milliseconds to a human readable format"
+  set -l milliseconds $argv[1]
+  set -l seconds (math "$milliseconds / 1000 % 60")
+  set -l minutes (math "$milliseconds / 60000 % 60")
+  set -l hours (math "$milliseconds / 3600000 % 24")
+  set -l days (math "$milliseconds / 86400000")
+  set -l time
+  set -l threshold 5
+
+  if test $days -gt 0
+    set time (command printf "$time%sd " $days)
+  end
+
+  if test $hours -gt 0
+    set time (command printf "$time%sh " $hours)
+  end
+
+  if test $minutes -gt 0
+    set time (command printf "$time%sm " $minutes)
+  end
+
+  if test $seconds -gt $threshold
+    set time (command printf "$time%ss " $seconds)
+  end
+
+  echo -e $time
 end
 
 function _in_git_directory
@@ -60,19 +88,30 @@ function fish_prompt
 
   _print_in_color "\n"(_pwd_with_tilde) blue
 
+  # If in git repo, show info about repo
   if _in_git_directory
     _print_in_color (__fish_git_prompt) cyan
     _print_in_color " "(_git_upstream_status) cyan
   end
 
+  # Show user@hostname if SSH'd in
   if set -q SSH_CONNECTION
-      _print_in_color " "(_prompt_hostname) 65737E
+    _print_in_color " "(_prompt_hostname) 65737E
   end
 
-  if set -q VIRTUAL_ENV
-      _print_in_color "\n("(basename "$VIRTUAL_ENV")")" normal
-      _print_in_color "❯ " (_prompt_color_for_status $last_status)
-  else
-      _print_in_color "\n❯ " (_prompt_color_for_status $last_status)
+  # Show process run time if longer than 5 seconds
+  if set -q CMD_DURATION
+    if test $CMD_DURATION -gt 5000
+        _print_in_color " "(_format_time $CMD_DURATION) yellow
+    end
   end
+
+  # Show prompt, with Python Virtualenv support
+  if set -q VIRTUAL_ENV
+    _print_in_color "\n("(basename "$VIRTUAL_ENV")")" normal
+    _print_in_color "❯ " (_prompt_color_for_status $last_status)
+  else
+    _print_in_color "\n❯ " (_prompt_color_for_status $last_status)
+  end
+
 end
